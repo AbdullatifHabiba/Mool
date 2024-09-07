343673b062d10577c1e7d924f6e6a2a4a36c5f93
AbdullatifHabiba <es-abdellatifkhalid2024@alexu.edu.eg>
2024-09-07 16:44:02 +0300

    Refactor filter options UI in FilterDetailScreen

--- lib/screens/home/filter_detiels_screen.dart
+++ lib/screens/home/filter_detiels_screen.dart
@@ -13,9 +13,42 @@ class FilterDetailScreen extends ConsumerWidget {
       appBar: AppBar(
         title: Text('Select $filterType'),
         automaticallyImplyLeading: false,
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              'Cancel',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
-      body: ListView(
-        children: _buildFilterOptions(context, ref),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: _buildFilterOptions(context, ref),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                _applyFilters(ref);
+                Navigator.of(context).pop();
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text('Apply'),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -43,6 +76,16 @@ class FilterDetailScreen extends ConsumerWidget {
     return options.map((option) {
       return ListTile(
         title: Text('$option$suffix'),
+        trailing: Checkbox(
+          value: ref.watch(filterProvider).containsValue(option),
+          onChanged: (bool? value) {
+            if (value == true) {
+              ref.read(filterProvider.notifier).setFilter(filterKey, option);
+            } else {
+              ref.read(filterProvider.notifier).setFilter(filterKey, '');
+            }
+          },
+        ),
         tileColor: Colors.white,
         shape: const Border(
           bottom: BorderSide(
@@ -51,9 +94,11 @@ class FilterDetailScreen extends ConsumerWidget {
           ),
         ),
         onTap: () {
-          ref.read(filterProvider.notifier).setFilter(filterKey, option);
-          _applyFilters(ref);
-          Navigator.of(context).pop();
+          if (ref.watch(filterProvider).containsValue(option) ) {
+            ref.read(filterProvider.notifier).setFilter(filterKey, '');
+          } else {
+            ref.read(filterProvider.notifier).setFilter(filterKey, option);
+          }
         },
       );
     }).toList();
